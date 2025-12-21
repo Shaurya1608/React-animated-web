8b28b56b8534ac67bb9500297f0206ea554a67a8
Shaurya1608 <shauryak1608@example.com>
2025-12-21 20:18:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Agence from './pages/Agence.jsx'
+import Project from './pages/Project.jsx'
+
+const App = () => {
+  return (
+    <div className='text-white' >
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/agence' element={<Agence/>}/>
+        <Route path='/project' element={<Project/>}/>
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
