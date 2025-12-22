594163c03f8eb8e09223aedcabb08f0ac672df6d
Shaurya1608 <shauryak1608@example.com>
2025-12-22 17:30:17 +0530

    half done of full-navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Agence from './pages/Agence.jsx'
 import Project from './pages/Project.jsx'
+import Navbar from './components/navigation/Navbar.jsx'
+import FullScreenNav from './components/navigation/FullScreenNav.jsx'
 
 
 const App = () => {
 
   return (
     <div  >
-      <Routes>
+      <FullScreenNav/>
+      {/* <Navbar/> */}
+      {/* <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/agence' element={<Agence/>}/>
         <Route path='/project' element={<Project/>}/>
-      </Routes>
+      </Routes> */}
 
     </div>
   )
---
+++ src/components/navigation/FullScreenNav.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div id='fullscreennav' className= 'text-white bg-black py-50 h-screen w-full absolute'>
+        <div className=''>
+            <div className='link relative border-t border-white'>
+                <h1 className='font-[font2] text-[8vw] text-center leading-[0.7] pt-7 uppercase'>Projects</h1>
+               <div className='moveLink text-black absolute flex top-0 bg-[#D3FD50]'>
+                 <div className='moveX flex  items-center'>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumbnail.png" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumb.jpg" alt="" />
+                </div>
+                <div className='moveX flex  items-center'>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumbnail.png" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumb.jpg" alt="" />
+                    
+                </div>
+                
+               </div>
+               
+            </div>
+            <div className='link relative border-t border-white'>
+                <h1 className='font-[font2] text-[8vw] text-center leading-[0.7] pt-7 uppercase'>Agency</h1>
+               <div className='moveLink text-black absolute flex top-0 bg-[#D3FD50]'>
+                 <div className='moveX flex  items-center'>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumbnail.png" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumb.jpg" alt="" />
+                </div>
+                <div className='moveX flex  items-center'>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumbnail.png" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumb.jpg" alt="" />
+                    
+                </div>
+                
+               </div>
+               
+            </div>
+            <div className='link relative border-t border-white'>
+                <h1 className='font-[font2] text-[8vw] text-center leading-[0.7] pt-7 uppercase'>Contact</h1>
+               <div className='moveLink text-black absolute flex top-0 bg-[#D3FD50]'>
+                 <div className='moveX flex  items-center'>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumbnail.png" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumb.jpg" alt="" />
+                </div>
+                <div className='moveX flex  items-center'>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumbnail.png" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumb.jpg" alt="" />
+                    
+                </div>
+                
+               </div>
+               
+            </div>
+            <div className='link relative border-y border-white'>
+                <h1 className='font-[font2] text-[8vw] text-center leading-[0.7] pt-7 uppercase'>Blog</h1>
+               <div className='moveLink text-black absolute flex top-0 bg-[#D3FD50]'>
+                 <div className='moveX flex  items-center'>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumbnail.png" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumb.jpg" alt="" />
+                </div>
+                <div className='moveX flex  items-center'>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumbnail.png" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.6] pt-10 uppercase'>To See Everything</h2>
+                    <img className='  object-cover h-24 shrink-0 w-60 rounded-full' src="/Thumbnail/Thumb.jpg" alt="" />
+                    
+                </div>
+                
+               </div>
+               
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
