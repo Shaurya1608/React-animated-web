3738b476e0b34ccaf8e71c8982e851cb4212cf2b
Shaurya1608 <shauryak1608@example.com>
2025-12-24 04:36:08 +0530

    final3

--- src/components/home/HomeBottonText.jsx
+++ src/components/home/HomeBottonText.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const HomeBottonText = () => {
   return (
     <div className='font-[font2] flex justify-center items-center gap-2 '>
-      <p className='absolute lg:w-80 w-60 lg:base text-xs right-0 lg:bottom-40 bottom-37 font-[font1] pr-2 leading-tight'>
+      <p className='absolute lg:w-80 w-60 lg:base lg:text-[1vw] text-xs right-0 lg:bottom-40 bottom-37 font-[font1] pr-2 leading-tight'>
       &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; K72 is an agency that carefully considers every action to nurture the brand. Tomorrow, in 5 months, and in 5 years. We seek out the friction that creates the spark to generate emotion. To ensure an honest relationship, we are unfiltered; we say what needs to be said, we do what needs to be done.
       </p>
       <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-25 flex items-center lg:px-10 px-7 border-white rounded-full uppercase'>
